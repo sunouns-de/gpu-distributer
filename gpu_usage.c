fae68a93badcc2bbc1fe119018ac2f8dd676031c
Fabian Pfaff <mysterytyp@gmail.com>
2025-08-08 01:54:15 +0200

    push it to tha limit

--- gpu_usage.c
+++ gpu_usage.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <nvml.h>
 
+int init_device(int device_num){
+    nvmlReturn_t result;
+    unsigned int device_count, i;
+
+    result = nvmlInit();
+    if (result != NVML_SUCCESS) {
+        printf("Failed to initialize NVML: %s\n", nvmlErrorString(result));
+        return 1;
+    }
+
+    result = nvmlDeviceGetCount(&device_count);
+    if (result != NVML_SUCCESS) {
+        printf("Failed to get device count: %s\n", nvmlErrorString(result));
+        nvmlShutdown();
+        return 1;
+    }
+
+    if (device_count < device_num){
+        return -1;
+    }
+    return 0;
+}
+
 int main() {
     nvmlReturn_t result;
     unsigned int device_count, i;
